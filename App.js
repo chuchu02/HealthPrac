3e0081791433864539f0f508545c5a423b7013cb
chuchu02 <rlaxogns73@gmail.com>
2022-10-19 10:47:39 +0900

    Navigation Test

--- App.js
+++ App.js
@@ -10,113 +10,36 @@ import {
   Alert,
   Image,
   ImageResizeMode,
+  Button,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import tw from 'twrnc';
+import Home from './Screen/Home';
+import Chest from './Screen/Chest';
+import Back from './Screen/Back';
+import Abs from './Screen/Abs';
+import LowerBody from './Screen/LowerBody';
+import Shoulder from './Screen/Shoulder';
 
-import {theme} from './colors';
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <>
       <NavigationContainer>
-        <View style={tw`p-4 bg-black flex-row justify-center flex-shrink-0`}>
-          <Text style={tw`text-xl text-[#FFF]`}>홈 트레이닝</Text>
-        </View>
-        <ScrollView style={tw`p-4 bg-white flex-grow`}>
-          <TouchableOpacity style={styles.HealthBox}>
-            <Text style={styles.HealthName}>가슴 운동</Text>
-            <Image
-              style={styles.HealthImageBox}
-              source={{
-                uri: 'https://ifh.cc/g/nPy47T.jpg',
-              }}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.HealthBox}>
-            <Text style={styles.HealthName}>등 운동</Text>
-            <Image
-              style={styles.HealthImageBox}
-              source={{
-                uri: 'https://ifh.cc/g/7Ogx5q.jpg',
-              }}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.HealthBox}>
-            <Text style={styles.HealthName}>복근 운동</Text>
-            <Image
-              style={styles.HealthImageBox}
-              source={{
-                uri: 'https://ifh.cc/g/6A7Ysc.jpg',
-              }}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.HealthBox}>
-            <Text style={styles.HealthName}>하체 운동</Text>
-            <Image
-              style={styles.HealthImageBox}
-              source={{
-                uri: 'https://ifh.cc/g/KCtwLO.jpg',
-              }}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.HealthBox}>
-            <Text style={styles.HealthName}>어깨 운동</Text>
-            <Image
-              style={styles.HealthImageBox}
-              source={{
-                uri: 'https://ifh.cc/g/1Fs4bR.jpg',
-              }}
-            />
-          </TouchableOpacity>
-        </ScrollView>
-        <View
-          style={tw`p-4 bg-green-500 flex-row justify-center flex-shrink-0`}>
-          <Text style={tw`text-xl text-[#000]`}>여기는 Footer 입니다.</Text>
-        </View>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Chest" component={Chest} />
+          <Stack.Screen name="Back" component={Back} />
+          <Stack.Screen name="Abs" component={Abs} />
+          <Stack.Screen name="LowerBody" component={LowerBody} />
+          <Stack.Screen name="Shoulder" component={Shoulder} />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: theme.bg,
-    paddingHorizontal: 20,
-  },
-  header: {
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    marginTop: 100,
-  },
-  HealthBox: {
-    width: '100%',
-    height: 140,
-    backgroundColor: 'black',
-    marginBottom: 10,
-    borderRadius: 25,
-    position: 'relative',
-  },
-  HealthImageBox: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    borderRadius: 25,
-  },
-  HealthName: {
-    color: 'white',
-    fontSize: 30,
-    opacity: 0.8,
-    fontWeight: '600',
-    position: 'absolute',
-    top: 0,
-    left: 10,
-    zIndex: 1,
-  },
-});
 
 export default App;
---
+++ Screen/Abs.js
@@ -0,0 +1,83 @@
+import {useEffect, useState} from 'react';
+import * as React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Alert,
+  Image,
+  ImageResizeMode,
+  Button,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import tw from 'twrnc';
+
+import {theme} from '../colors';
+
+const Abs = () => {
+  return (
+    <>
+      <View style={tw`p-4 bg-black flex-row justify-center flex-shrink-0`}>
+        <Text style={tw`text-xl text-[#FFF]`}>홈 트레이닝</Text>
+      </View>
+      <ScrollView style={tw`p-4 bg-white flex-grow`}>
+        <TouchableOpacity style={styles.HealthBox}>
+          <Text style={styles.HealthName}>복근 운동</Text>
+          <Image
+            style={styles.HealthImageBox}
+            source={{
+              uri: 'https://ifh.cc/g/6A7Ysc.jpg',
+            }}
+          />
+        </TouchableOpacity>
+      </ScrollView>
+      <View style={tw`p-4 bg-green-500 flex-row justify-center flex-shrink-0`}>
+        <Text style={tw`text-xl text-[#000]`}>여기는 Footer 입니다.</Text>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.bg,
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 100,
+  },
+  HealthBox: {
+    width: '100%',
+    height: 140,
+    backgroundColor: 'black',
+    marginBottom: 10,
+    borderRadius: 25,
+    position: 'relative',
+  },
+  HealthImageBox: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    borderRadius: 25,
+  },
+  HealthName: {
+    color: 'white',
+    fontSize: 30,
+    opacity: 0.8,
+    fontWeight: '600',
+    position: 'absolute',
+    top: 0,
+    left: 10,
+    zIndex: 1,
+  },
+});
+
+export default Abs;
